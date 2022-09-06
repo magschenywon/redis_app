f62ff2af63176f53dd451fc8d568ce46b0171c87
magschenywon <37623036+magschenywon@users.noreply.github.com>
2022-09-05 20:48:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,22 +17,7 @@ def show_list():
               for row in US_COUNTRIES]
     return render_template('index.html', appproject=database)
 
-'''
-@app.route("/add", methods=['POST'])
-def add_entry():
-    db = get_db()
-    db.execute('insert into US_COUNTRIES (date, county, state, flips, cases, deaths) values (?,?,?,?,?,?)',
-               [request.form['date'], request.form['county'],request.form ['state'],request.form['flips'],request.form['cases'],request.form['deaths']])
-    db.commit()
-    return redirect(url_for('show_list'))
 
-@app.route("/delete/<item>")
-def delete_entry(item):
-    db = get_db()
-    db.execute("DELETE FROM US_COUNTRIES WHERE date='"+item+"'OR county='"+item+"' OR state='"+item+"' flips = '"+item+"' OR cases='"+item+"' OR deaths ='"+item+"'" )
-    db.commit()
-    return redirect(url_for('show_list'))
-'''
 
 def get_db():
     if 'DATABASE' not in g:
@@ -43,29 +28,14 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-'''
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'CSVDATA_db'):
-        g.CSVDATA_db = sqlite3.connect(app.config['DATABASE'])
-    return g.sqlite_db
-'''
+
 
 def close_db(e=None):
     db = g.pop('', None)
 
     if db is not None:
         db.close()
-'''
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
 
-'''
 if __name__ == "_main_":
     app.run("0.0.0.0",port=80)
 
